d08743975ec0416306260d8e9951b7206da25686
Martin Carolan <martin.carolan@itv.com>
2016-04-25 09:33:49 +0100

    Fix integration test, and add integration test for message headers being exposed to raw consumers

--- src/it/scala/itv/bucky/ConsumerIntegrationTest.scala
+++ src/it/scala/itv/bucky/ConsumerIntegrationTest.scala
@@ -1,21 +1,26 @@
 package itv.bucky
 
+import itv.akka.rmq.{AmqpMessageProperties, AmqpMessage}
 import itv.contentdelivery.testutilities.SameThreadExecutionContext.implicitly
 import itv.utils.Blob
-import org.scalatest.FunSuite
+import org.scalatest.{Inside, FunSuite}
 import org.scalatest.Matchers._
 import org.scalatest.concurrent.Eventually.eventually
 import org.scalatest.concurrent.ScalaFutures
+import Inside._
+
+import scala.collection.JavaConverters._
 
 class ConsumerIntegrationTest extends FunSuite with ScalaFutures {
 
-  lazy val (Seq(consumerQueue, rawConsumerQueue), amqpClientConfig, rmqAdminHhttp) = IntegrationUtils.setUp("bucky-consumer-test", "bucky-consumer-raw-test")
+  lazy val (Seq(consumerQueue, rawConsumerQueue, headersQueue), amqpClientConfig, rmqAdminHhttp) = IntegrationUtils.setUp("bucky-consumer-test", "bucky-consumer-raw-test", "bucky-consumer-headers-test")
 
 
   case class Message(value: String)
   implicit val messageDeserializer = new BlobDeserializer[Message] {
     override def apply(blob: Blob): DeserializerResult[Message] = DeserializerResult.Success(Message(blob.to[String]))
   }
+  
   ignore("Can consume messages from a (pre-existing) queue") {
     consumerQueue.purge()
 
@@ -52,8 +57,33 @@ class ConsumerIntegrationTest extends FunSuite with ScalaFutures {
 
       eventually {
         handler.receivedMessages should have size 1
-        handler.receivedMessages.head shouldBe msg
+        inside(handler.receivedMessages.head) {
+          case Delivery(body, _, _, _) => body shouldBe msg
+        }
       }
     }
   }
+
+  ignore("Message headers are exposed to (raw) consumers") {
+    headersQueue.purge()
+
+    val handler = new StubHandler[Delivery]()
+    for {
+      amqpClient <- amqpClientConfig
+      consumer <- amqpClient.consumer(headersQueue.name, handler)
+    } {
+      handler.receivedMessages shouldBe 'empty
+
+      val msg = Blob from "Hello World!"
+      headersQueue.publishRaw(AmqpMessage(msg, AmqpMessageProperties(Some(Map("hello" -> "world")))))
+
+      eventually {
+        handler.receivedMessages should have size 1
+        inside(handler.receivedMessages.head) {
+          case Delivery(body, _, _, properties) => properties.getHeaders.asScala.get("hello").map(_.toString) shouldBe Some("world")
+        }
+      }
+    }
+  }
+
 }
