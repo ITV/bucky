4b8e44b93fcf7333ebab2a077d69b799337a1ee7
Martin Carolan <martin.carolan@itv.com>
2017-05-26 08:45:22 +0100

    Trigger first ci build

--- ci/publish.sh
+++ ci/publish.sh
@@ -3,6 +3,12 @@
 #exit non-zero return code if a simple command exits with non-zero return code
 set -e
 
+echo "TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST"
+echo "TRAVIS_BRANCH = $TRAVIS_BRANCH"
+echo "TRAVIS_TAG = $TRAVIS_TAG"
+version=`cat version.sbt`
+echo "version = $version"
+
 sbt_cmd="sbt ++$TRAVIS_SCALA_VERSION"
 
 eval "$sbt_cmd clean compile test"
