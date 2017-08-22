4d1b779b0ff3e3b7380d9a3dde09ff09468892f8
benivf <beni.villa@gmail.com>
2017-08-22 09:23:08 +0100

    Fix the docs for scala 11

--- example/src/main/scala/com/itv/bucky/example/marshalling/UnmarshallingConsumer.scala
+++ example/src/main/scala/com/itv/bucky/example/marshalling/UnmarshallingConsumer.scala
@@ -42,7 +42,7 @@ object UnmarshallingConsumer extends App with StrictLogging {
         UnmarshalResult.Failure(s"Expected message to be in format <name>,<age>: got '$csvString'")
     }
 
-  val personUnmarshaller: PayloadUnmarshaller[Person] = StringPayloadUnmarshaller.flatMap(csvStringToPerson(_))
+  val personUnmarshaller: PayloadUnmarshaller[Person] = StringPayloadUnmarshaller.flatMap(x => csvStringToPerson(x))
   //end snippet 2
 
   //start snippet 3
