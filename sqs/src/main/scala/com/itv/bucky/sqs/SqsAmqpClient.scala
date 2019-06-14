9e181e26afab5f647b851b4a578049c19e81ee39
Chris Chivers <chris.chivers@itv.com>
2019-06-14 12:03:54 +0100

    WIP

--- sqs/src/main/scala/com/itv/bucky/sqs/SqsAmqpClient.scala
+++ sqs/src/main/scala/com/itv/bucky/sqs/SqsAmqpClient.scala
@@ -1,23 +1,26 @@
 package com.itv.bucky.sqs
 
-import cats.effect.{ConcurrentEffect, ExitCode, IO, IOApp, Resource}
-import com.amazonaws.services.sqs.{AmazonSQS, AmazonSQSClientBuilder}
-import com.amazonaws.services.sqs.model.SendMessageRequest
-import com.itv.bucky
-import com.itv.bucky.{AmqpClient, ExchangeName, Handler, MessagePropertiesConverters, Payload, Publisher, QueueName, RoutingKey, consume, decl}
+import cats.arrow.FunctionK
+import cats.effect._
 import cats.implicits._
-import com.itv.bucky.consume.{Ack, ConsumeAction, PublishCommand}
+import com.amazonaws.services.sqs.buffered.AmazonSQSBufferedAsyncClient
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest
+import com.amazonaws.services.sqs.{AmazonSQS, AmazonSQSAsyncClient, AmazonSQSAsyncClientBuilder, AmazonSQSClientBuilder}
+import com.itv.bucky
+import com.itv.bucky.consume.{Ack, ConsumeAction, ConsumerTag, Delivery, PublishCommand}
 import com.itv.bucky.publish.MessageProperties
-import com.rabbitmq.client.Delivery
+import com.itv.bucky.{AmqpClient, Envelope, ExchangeName, Handler, Payload, Publisher, QueueName, RoutingKey, consume, decl}
 
+import scala.collection.JavaConverters._
 import scala.language.higherKinds
 
 object SqsAmqpClient {
 
   //https://sqs.eu-west-1.amazonaws.com/829282787238/bucky-sqs-test-queue
 
-  def apply[F[_]](clientBuilder: AmazonSQSClientBuilder)(implicit F: ConcurrentEffect[F]): Resource[F, AmqpClient[F]] = {
-    val client: Resource[F, AmazonSQS] = Resource.make(F.delay(clientBuilder.build()))(c => F.delay(c.shutdown()))
+  def apply[F[_]](clientBuilder: AmazonSQSAsyncClientBuilder)(implicit F: ConcurrentEffect[F], contextShift: ContextShift[IO]): Resource[F, AmqpClient[F]] = {
+    val client: Resource[F, AmazonSQSBufferedAsyncClient] = Resource.make(F.delay(clientBuilder.build()))(c => F.delay(c.shutdown()))
+      .map(new AmazonSQSBufferedAsyncClient(_))
     client.map { sqs =>
       new AmqpClient[F] {
         override def declare(declarations: decl.Declaration*): F[Unit] = ???
@@ -29,10 +32,29 @@ object SqsAmqpClient {
           ()
         }
 
-        override def registerConsumer(queueName: bucky.QueueName, handler: Handler[F, consume.Delivery], exceptionalAction: consume.ConsumeAction, prefetchCount: Int): Resource[F, Unit] = ???
+        override def registerConsumer(queueName: bucky.QueueName, handler: Handler[F, consume.Delivery], exceptionalAction: consume.ConsumeAction, prefetchCount: Int): Resource[F, Unit] = {
+          Resource.make(F.toIO(F.delay(awaitMessages(queueName, handler))).start)(_.cancel).mapK[F](FunctionK.lift[IO, F](F.liftIO)).map(_ => ())
+        }
 
         override def isConnectionOpen: F[Boolean] = ???
+
+        private def awaitMessages(queueName: QueueName, handler: Handler[F, consume.Delivery]) = {
+          while (true) {
+            val messageRequest = new ReceiveMessageRequest()
+              .withMaxNumberOfMessages(1)
+              .withQueueUrl(queueName.value)
+            val result = sqs.receiveMessage(messageRequest)
+            result.getMessages.asScala.headOption.foreach{ msg =>
+              val delivery = Delivery(Payload.from[String](msg.getBody), ConsumerTag("foo"), Envelope(0L, redeliver = false, ExchangeName(""), RoutingKey(queueName.value)), MessageProperties.basic)
+              F.toIO[ConsumeAction](handler.apply(delivery)).unsafeRunSync()
+            }
+          }
+        }
       }
+
+
+
+
     }
   }
 
@@ -47,8 +69,7 @@ object Main extends IOApp {
   }
 
   override def run(args: List[String]): IO[ExitCode] = {
-    val builder =
-      AmazonSQSClientBuilder.standard()
+    val builder = AmazonSQSAsyncClientBuilder.standard()
     builder.setRegion("eu-west-1")
 
     val queueName = "bucky-sqs-test-queue"
@@ -63,7 +84,7 @@ object Main extends IOApp {
     clientResource.use { amqpClient =>
       val publisher = amqpClient.publisher()
       publisher(PublishCommand(ExchangeName(""), RoutingKey(queueName), MessageProperties.basic, Payload.from[String]("hello, world!")))
-    } *> IO.pure(ExitCode.Success)
+    } *> IO.never.as(ExitCode.Success)
   }
 
 }
