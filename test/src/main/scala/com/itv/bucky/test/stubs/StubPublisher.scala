e665ea5d41b31e38aa32a775a0ffc370162db97e
Andrew Gee <andrew@andrewgee.net>
2020-08-20 14:55:32 +0100

    StubPublisherWithHeaders
    Co-authored-by: Andrea Magnorsky <andrea.magnorsky1@itv.com>
    Co-authored-by: Sarah Edwards <sarah.edwards1@itv.com>

--- test/src/main/scala/com/itv/bucky/test/stubs/StubPublisher.scala
+++ test/src/main/scala/com/itv/bucky/test/stubs/StubPublisher.scala
@@ -1,8 +1,9 @@
 package com.itv.bucky.test.stubs
 
 import cats.effect.Sync
-import com.itv.bucky.Publisher
+import com.itv.bucky.{Publisher, PublisherWithHeaders}
 import cats.implicits._
+
 import scala.collection.mutable.ListBuffer
 import scala.language.higherKinds
 
@@ -10,4 +11,12 @@ class StubPublisher[F[_], T](implicit F: Sync[F]) extends Publisher[F, T] {
   private val published: ListBuffer[T] = ListBuffer[T]()
   def recordedMessages: List[T]        = published.synchronized(published.toList)
   override def apply(v1: T): F[Unit]   = F.delay(published.synchronized(published += v1)).void
+}
+
+class StubPublisherWithHeaders[F[_], T](implicit F: Sync[F]) extends PublisherWithHeaders[F, T] {
+  private val published: ListBuffer[(T, Map[String, AnyRef])] = ListBuffer[(T, Map[String, AnyRef])]()
+  def recordedMessages: List[(T, Map[String, AnyRef])]        = published.synchronized(published.toList)
+
+  override def apply(v1: T, v2: Map[String, AnyRef]): F[Unit] =
+    F.delay(published.synchronized(published += ((v1, v2)))).void
 }
