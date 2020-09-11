e43e79c830140c26e95febc4be6d8ac34c4afd4f
JoeCordingley <joewcordingley@gmail.com>
2020-09-11 14:11:52 +0100

    wip

--- http4s-document/src/main/scala/com/itv/bucky/document/package.scala
+++ http4s-document/src/main/scala/com/itv/bucky/document/package.scala
@@ -1,23 +1,26 @@
 package com.itv.bucky
 
 import cats.effect.Sync
-import com.itv.bucky.wiring.Wiring
-import com.itv.bucky.wiring.implicits._
 import language.higherKinds
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 
 package object document {
 
+  case class RMQPublishRoute(exchangeName: ExchangeName, routingKey: RoutingKey, schema: String)
+  object RMQPublishRoute {
+    def document(r: RMQPublishRoute): String = s"ExchangeName: ${r.exchangeName.value}\nRoutingKey: ${r.routingKey.value}\nPayload: ${r.schema}"
+  }
+
   trait Documentation[F[_]] {
     def getRabbitDocs: F[String]
   }
 
   object Documentation {
-    def apply[F[_]: Sync](producers: List[Wiring[_]]): Documentation[F] = new Documentation[F] {
+    def apply[F[_]: Sync](routes: List[RMQPublishRoute]): Documentation[F] = new Documentation[F] {
       override def getRabbitDocs: F[String] =
         Sync[F].delay(
-          s"Producers: ${producers.map(_.document).mkString("\n---------------\n")}"
+          s"Producers: ${routes.map(RMQPublishRoute.document).mkString("\n---------------\n")}"
         )
       // Return list of producers
       // Return list of consumers
