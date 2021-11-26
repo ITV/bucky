3cab4746ea36c616b918c03eb98dca75d5f0c56b
Martin Carolan <mail@mcarolan.net>
2021-11-26 15:00:09 +0000

    Change to EffectTestSupport

--- test/src/it/scala/HammerTest.scala
+++ test/src/it/scala/HammerTest.scala
@@ -20,8 +20,9 @@ import scala.collection.immutable.TreeSet
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import scala.language.higherKinds
+import cats.effect.testing.scalatest.EffectTestSupport
 
-class HammerTest extends AsyncFunSuite with GlobalAsyncIOSpec with Eventually with IntegrationPatience with StrictLogging with Matchers {
+class HammerTest extends AsyncFunSuite with EffectTestSupport with Eventually with IntegrationPatience with StrictLogging with Matchers {
 
   case class TestFixture(stubHandler: RecordingHandler[IO, String], publisher: Publisher[IO, String], client: AmqpClient[IO])
 
@@ -52,13 +53,14 @@ class HammerTest extends AsyncFunSuite with GlobalAsyncIOSpec with Eventually wi
         rawConfig.getString("rmq.host"),
         rawConfig.getInt("rmq.port"),
         rawConfig.getString("rmq.username"),
-        rawConfig.getString("rmq.password"))
-    implicit val payloadMarshaller: PayloadMarshaller[String] = StringPayloadMarshaller
+        rawConfig.getString("rmq.password")
+      )
+    implicit val payloadMarshaller: PayloadMarshaller[String]     = StringPayloadMarshaller
     implicit val payloadUnmarshaller: PayloadUnmarshaller[String] = StringPayloadUnmarshaller
 
     val exchangeName = ExchangeName(UUID.randomUUID().toString)
-    val routingKey = RoutingKey(UUID.randomUUID().toString)
-    val queueName = QueueName(UUID.randomUUID().toString)
+    val routingKey   = RoutingKey(UUID.randomUUID().toString)
+    val queueName    = QueueName(UUID.randomUUID().toString)
 
     val declarations = List(
       Queue(queueName).autoDelete.expires(20.minutes),
@@ -71,7 +73,7 @@ class HammerTest extends AsyncFunSuite with GlobalAsyncIOSpec with Eventually wi
         Resource.eval(client.declare(declarations)).flatMap(_ => client.registerConsumerOf(queueName, handler))
 
       handlerResource.use { _ =>
-        val pub = client.publisherOf[String] (exchangeName, routingKey)
+        val pub     = client.publisherOf[String](exchangeName, routingKey)
         val fixture = TestFixture(handler, pub, client)
         test(fixture)
       }
@@ -80,31 +82,32 @@ class HammerTest extends AsyncFunSuite with GlobalAsyncIOSpec with Eventually wi
 
   test("can handle concurrency") {
     withTestFixture { testFixture =>
-      val hammerStrength = 10000
+      val hammerStrength  = 10000
       val parallelPublish = 250
 
       val results =
         (1 to hammerStrength)
           .grouped(parallelPublish)
           .toList
-          .flatTraverse(group => {
-            group.toList.parTraverse { i =>
-              testFixture.publisher(s"hello$i").attempt.map {
-                case Right(_) => None
-                case Left(error) => Some(error)
+          .flatTraverse { group =>
+            group.toList
+              .parTraverse { i =>
+                testFixture.publisher(s"hello$i").attempt.map {
+                  case Right(_)    => None
+                  case Left(error) => Some(error)
+                }
               }
-            }.map(_.flatten)
-          })
+              .map(_.flatten)
+          }
 
       for {
         errors <- results
+      } yield eventually {
+        errors shouldBe empty
+        testFixture.stubHandler.receivedMessages should have size hammerStrength
+        val set = TreeSet(testFixture.stubHandler.receivedMessages: _*)
+        (1 to hammerStrength).foreach(i => set.contains(s"hello$i") shouldBe true)
       }
-        yield eventually {
-          errors shouldBe empty
-          testFixture.stubHandler.receivedMessages should have size hammerStrength
-          val set = TreeSet(testFixture.stubHandler.receivedMessages: _*)
-          (1 to hammerStrength).foreach(i => set.contains(s"hello$i") shouldBe true)
-        }
     }
   }
 
@@ -114,30 +117,28 @@ class HammerTest extends AsyncFunSuite with GlobalAsyncIOSpec with Eventually wi
     withTestFixture { testFixture =>
       implicit val stringPayloadMarshaller: PayloadMarshaller[String] = StringPayloadMarshaller
 
-      val exchange = ExchangeName("anexchange")
+      val exchange  = ExchangeName("anexchange")
       val slowQueue = QueueName("aqueue1" + UUID.randomUUID().toString)
-      val slowRk = RoutingKey("ark1")
+      val slowRk    = RoutingKey("ark1")
       val fastQueue = QueueName("aqueue2" + UUID.randomUUID().toString)
-      val fastRk = RoutingKey("ark2")
+      val fastRk    = RoutingKey("ark2")
 
       val order: Ref[IO, List[String]] = Ref.of[IO, List[String]](List.empty).unsafeRunSync()
 
       val fastPublisher = testFixture.client.publisherOf[String](exchange, fastRk)
       val slowPublisher = testFixture.client.publisherOf[String](exchange, slowRk)
 
-      val fastHandler = new RecordingHandler[IO, String]((v1: String) => {
+      val fastHandler = new RecordingHandler[IO, String]((v1: String) =>
         for {
           _ <- order.update(_ :+ "fast")
-        }
-          yield Ack
-      })
-      val slowHandler = new RecordingHandler[IO, String]((v1: String) => {
+        } yield Ack
+      )
+      val slowHandler = new RecordingHandler[IO, String]((v1: String) =>
         for {
           _ <- IO.sleep(10.second)
           _ <- order.update(_ :+ "slow")
-        }
-          yield Ack
-      })
+        } yield Ack
+      )
 
       val declarations =
         List(Queue(slowQueue), Queue(fastQueue), Exchange(exchange).binding(slowRk -> slowQueue).binding(fastRk -> fastQueue))
@@ -146,8 +147,7 @@ class HammerTest extends AsyncFunSuite with GlobalAsyncIOSpec with Eventually wi
         for {
           _ <- testFixture.client.registerConsumerOf(slowQueue, slowHandler)
           _ <- testFixture.client.registerConsumerOf(fastQueue, fastHandler)
-        }
-          yield ()
+        } yield ()
 
       val handlersResource =
         Resource.eval(testFixture.client.declare(declarations)).flatMap(_ => handlers)
@@ -157,19 +157,13 @@ class HammerTest extends AsyncFunSuite with GlobalAsyncIOSpec with Eventually wi
           _ <- slowPublisher("slow one")
           _ <- IO.sleep(1.second)
           _ <- fastPublisher("fast one")
+        } yield eventually {
+          val messages = order.get.unsafeRunSync()
+          messages should have size 2
+          messages shouldBe List("fast", "slow")
         }
-          yield {
-            eventually {
-              val messages = order.get.unsafeRunSync()
-              messages should have size 2
-              messages shouldBe List("fast", "slow")
-            }
-          }
       }
     }
   }
 
-
 }
-
-
