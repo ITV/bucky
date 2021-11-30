1ca67fcfadde9b39412efa53b4964af4eb85d313
Gregory Clifford <GregClifford>
2021-11-30 14:37:21 +0000

    Tidying

--- test/src/test/scala/com/itv/bucky/test/GlobalAsyncIOSpec.scala
+++ test/src/test/scala/com/itv/bucky/test/GlobalAsyncIOSpec.scala
@@ -1,15 +1,8 @@
 package com.itv.bucky.test
 
-import cats.effect.IO
-import cats.effect.testing.RuntimePlatform
+import cats.effect.testing.scalatest.{AssertingSyntax, EffectTestSupport}
 import cats.effect.unsafe.IORuntime
-
-import org.scalactic.source.Position
 import org.scalatest.AsyncTestSuite
-import org.scalatest.enablers.Retrying
-import org.scalatest.time.Span
-import cats.effect.testing.scalatest.AssertingSyntax
-import cats.effect.testing.scalatest.EffectTestSupport
 
 trait GlobalAsyncIOSpec extends AssertingSyntax with EffectTestSupport { asyncTestSuite: AsyncTestSuite =>
 
